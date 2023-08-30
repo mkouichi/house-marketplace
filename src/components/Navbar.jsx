078818160f22b99804ffc8288d2203406cd1b196
mkouichi <kic.mhr.qs@gmail.com>
2023-08-30 16:42:23 -0400

    Use NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,31 @@
-import { useLocation, useNavigate } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+import { ReactComponent as OfferIcon } from '../assets/svg/localOfferIcon.svg';
 import { ReactComponent as ExploreIcon } from '../assets/svg/exploreIcon.svg';
-import { ReactComponent as OfferIcon } from '../assets//svg/localOfferIcon.svg';
 import { ReactComponent as PersonOutlineIcon } from '../assets/svg/personOutlineIcon.svg';
 
 function Navbar() {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  // Check if the route is the same as the location
-  // To change icon and text color
-  const pathMatchRoute = (route) => {
-    if (route === location.pathname) {
-      return true;
-    }
-  };
-
   return (
     <footer className='navbar'>
       <nav className='navbarNav'>
         <ul className='navbarListItems'>
-          <li className='navbarListItem' onClick={() => navigate('/')}>
-            <ExploreIcon
-              fill={pathMatchRoute('/') ? '#2c2c2c' : '#8f8f8f'}
-              width='36px'
-              height='36px'
-              className='icon'
-            />
-            <p
-              className={
-                pathMatchRoute('/')
-                  ? 'navbarListItemNameActive'
-                  : 'navbarListItemName'
-              }>
-              Explore
-            </p>
+          <li>
+            <NavLink to='/' className='navbarListItemLink'>
+              <ExploreIcon fill='#8f8f8f' width='36px' height='36px' />
+              <p className='navbarListItemName'>Explore</p>
+            </NavLink>
           </li>
-          <li className='navbarListItem' onClick={() => navigate('/offers')}>
-            <OfferIcon
-              fill={pathMatchRoute('/offers') ? '#2c2c2c' : '#8f8f8f'}
-              width='36px'
-              height='36px'
-              className='icon'
-            />
-            <p
-              className={
-                pathMatchRoute('/offers')
-                  ? 'navbarListItemNameActive'
-                  : 'navbarListItemName'
-              }>
-              Offer
-            </p>
+          <li>
+            <NavLink to='/offers' className='navbarListItemLink'>
+              <OfferIcon fill='#8f8f8f' width='36px' height='36px' />
+              <p className='navbarListItemName'>Offers</p>
+            </NavLink>
           </li>
-          <li className='navbarListItem' onClick={() => navigate('/profile')}>
-            <PersonOutlineIcon
-              fill={pathMatchRoute('/profile') ? '#2c2c2c' : '#8f8f8f'}
-              width='36px'
-              height='36px'
-              className='icon'
-            />
-            <p
-              className={
-                pathMatchRoute('/profile')
-                  ? 'navbarListItemNameActive'
-                  : 'navbarListItemName'
-              }>
-              Profile
-            </p>
+          <li>
+            <NavLink to='/profile' className='navbarListItemLink'>
+              <PersonOutlineIcon fill='#8f8f8f' width='36px' height='36px' />
+              <p className='navbarListItemName'>Profile</p>
+            </NavLink>
           </li>
         </ul>
       </nav>
