4b1f227dd96e3a843bc12eedb1252def16e74871
mkouichi <kic.mhr.qs@gmail.com>
2023-08-31 14:58:57 -0400

    Add alerts with React-Toastify

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { Outlet } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import Navbar from './components/Navbar';
 
 function App() {
@@ -6,6 +9,7 @@ function App() {
     <>
       <Navbar />
       <Outlet />
+      <ToastContainer />
     </>
   );
 }
