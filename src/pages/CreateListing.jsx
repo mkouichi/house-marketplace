6daf00b6f6b7645f3f6799c891e4baee4aadeb5a
mkouichi <kic.mhr.qs@gmail.com>
2023-09-08 19:19:13 -0400

    - Return unsubscribe function instead of isMounted ref (remove the Firebase listener to stop memory leaks) - Declare initialFormState to prevent an infinite loop

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -1,42 +1,43 @@
-import { useEffect, useState, useRef } from 'react';
+import { useEffect, useState } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom';
 
 import Spinner from '../components/Spinner';
 
+// Declaring outside of the component guarantees it doesn't change and so stays the same Object reference in memory and won't trigger a useEffect
+const initialFormState = {
+  type: 'rent',
+  name: '',
+  bedrooms: 1,
+  bathrooms: 1,
+  parking: false,
+  furnished: false,
+  address: '',
+  offer: false,
+  regularPrice: 0,
+  discountedPrice: 0,
+  images: [],
+  latituude: 0,
+  longitude: 0,
+};
+
 function CreateListing() {
   const [geolocationEnabled, setGeolocationEnabled] = useState(true);
   const [loading, setLoading] = useState(false);
-  const [formData, setFormData] = useState({
-    type: 'rent',
-    name: '',
-    bedrooms: 1,
-    bathrooms: 1,
-    parking: false,
-    furnished: false,
-    address: '',
-    offer: false,
-    regularPrice: 0,
-    discountedPrice: 0,
-    images: [],
-    latituude: 0,
-    longitude: 0,
-  });
+  const [formData, setFormData] = useState(initialFormState);
 
   const auth = getAuth();
   const navigate = useNavigate();
-  const isMounted = useRef(true);
 
   useEffect(() => {
-    isMounted &&
-      onAuthStateChanged(auth, (user) => {
-        user
-          ? setFormData({ ...formData, userRef: user.uid })
-          : navigate('/sign-in');
-      });
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      user
+        ? setFormData({ ...initialFormState, userRef: user.uid })
+        : navigate('/sign-in');
+    });
 
-    return () => (isMounted.current = false);
-  }, [isMounted]);
+    return unsubscribe;
+  }, [auth, navigate]);
 
   if (loading) return <Spinner />;
 
